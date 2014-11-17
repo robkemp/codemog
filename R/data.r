7701cc56ae7f2914e829723b6bc59c0c10742972
robkemp <rjameskemp@gmail.com>
2014-11-17 07:57:30 -0700

    Updated Data Dictionary to vignette standards and other documentation files.

--- R/data.r
+++ R/data.r
@@ -5,6 +5,7 @@
 #' @format A data frame with nine variables: \code{geonum}, \code{countyfips},
 #'   \code{county}, \code{year}, \code{groupquartersPopulation}, \code{householdPopulation},\code{occupiedHousehold},
 #'    \code{totalHousingUnits}, and \code{totalPopulation}.
+#' @details See 'Data Dictionary' vignette for specifics on the dataset
 "county_est"
 
 #' Municipal Estimates
@@ -14,6 +15,7 @@
 #' @format A data frame with nine variables: \code{geonum}, \code{placefips},
 #'   \code{municipality}, \code{year}, \code{groupquartersPopulation}, \code{householdPopulation},\code{occupiedHousehold},
 #'    \code{totalHousingUnits}, and \code{totalPopulation}.
+#' @details See 'Data Dictionary' vignette for specifics on the dataset
 "muni_est"
 
 #' Municipalities within County Estimates
@@ -25,6 +27,7 @@
 #' \code{placefips}, \code{municipality}, \code{year}, \code{groupquartersPopulation}, 
 #' \code{householdPopulation},\code{occupiedHousehold}, \code{totalHousingUnits}, 
 #' and \code{totalPopulation}.
+#' @details See 'Data Dictionary' vignette for specifics on the dataset
 "muni_win_est"
 
 #' County by Age and Sex Estimates and Forecasts
@@ -35,4 +38,5 @@
 #' @format A data frame with nine variables: \code{countyfips},\code{year}, \code{age}, 
 #'   \code{county},  \code{male Population}, \code{femalePopulation}, 
 #'   \code{totalPopulation}, and \code{datatype}.
+#' @details See 'Data Dictionary' vignette for specifics on the dataset
 "county_forecast"
