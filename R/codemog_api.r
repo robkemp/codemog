0a89f706995d5a9f14c74d176fc030c5ba9b916d
robkemp <rjameskemp@gmail.com>
2015-02-03 09:10:16 -0700

    Collapsed age categories for ms_census_age

--- R/codemog_api.r
+++ R/codemog_api.r
@@ -105,48 +105,40 @@ ms_census_age=function(fips, state="08", base=12){
   d10[,7:56]=as.numeric(as.character(d10[,7:56]))
   d00[,7:56]=as.numeric(as.character(d00[,7:56]))
   d10c=d10%>%
-    mutate(age1=p12003+p12027,
-           age2=p12004+p12028,
-           age3=p12005+p12029,
-           age4=p12006+p12007+p12030+p12031,
-           age5=p12008+p12009+p12010+p12032+p12033+p12034,
-           age6=p12011+p12012+p12035+p12036,
-           age7=p12013+p12014+p12037+p12038,
-           age8=p12015+p12016+p12039+p12040,
-           age9=p12017+p12018+p12019+p12041+p12042+p12043,
-           age10=p12020+p12021+p12022+p12023+p12024+p12025+
+    mutate(age1=p12003+p12027+p12004+p12028,
+           age2=p12005+p12029+p12006+p12030,
+           age3=p12007+p12031+p12008+p12009+p12010+p12032+p12033+p12034,
+           age4=p12011+p12012+p12035+p12036,
+           age5=p12013+p12014+p12037+p12038,
+           age6=p12015+p12016+p12039+p12040,
+           age7=p12017+p12018+p12019+p12041+p12042+p12043,
+           age8=p12020+p12021+p12022+p12023+p12024+p12025+
              p12044+p12045+p12046+p12047+p12048+p12049)%>%
-    select(geoname:geonum,age1:age10)%>%
+    select(geoname:geonum,age1:age8)%>%
     melt(id=c("geoname", "state", "county", "place", "tract", "bg", "geonum"))%>%
     mutate(
       agecat=ordered(as.factor(variable), levels=c("age1", "age2", "age3", "age4", 
-                                                   "age5", "age6", "age7", "age8",
-                                                   "age9", "age10"), 
-                     labels=c("Less than 5", "5 to 9",
-                              "10 to 14", "15 to 19", "20 to 24", "25 to 34","35 to 44", 
+                                                   "age5", "age6", "age7", "age8"), 
+                     labels=c("Less than 9", "10 to 17", "18 to 24", "25 to 34","35 to 44", 
                               "45 to 54", "55 to 64", "65 and Over")),
       year="2010",
       geoname=stri_trans_general(geoname, id="Title"))
   d00c=d00%>%
-    mutate(age1=p12003+p12027,
-           age2=p12004+p12028,
-           age3=p12005+p12029,
-           age4=p12006+p12007+p12030+p12031,
-           age5=p12008+p12009+p12010+p12032+p12033+p12034,
-           age6=p12011+p12012+p12035+p12036,
-           age7=p12013+p12014+p12037+p12038,
-           age8=p12015+p12016+p12039+p12040,
-           age9=p12017+p12018+p12019+p12041+p12042+p12043,
-           age10=p12020+p12021+p12022+p12023+p12024+p12025+
+    mutate(age1=p12003+p12027+p12004+p12028,
+           age2=p12005+p12029+p12006+p12030,
+           age3=p12007+p12031+p12008+p12009+p12010+p12032+p12033+p12034,
+           age4=p12011+p12012+p12035+p12036,
+           age5=p12013+p12014+p12037+p12038,
+           age6=p12015+p12016+p12039+p12040,
+           age7=p12017+p12018+p12019+p12041+p12042+p12043,
+           age8=p12020+p12021+p12022+p12023+p12024+p12025+
              p12044+p12045+p12046+p12047+p12048+p12049)%>%
-    select(geoname:geonum,age1:age10)%>%
+    select(geoname:geonum,age1:age8)%>%
     melt(id=c("geoname", "state", "county", "place", "tract", "bg", "geonum"))%>%
     mutate(
       agecat=ordered(as.factor(variable), levels=c("age1", "age2", "age3", "age4", 
-                                                   "age5", "age6", "age7", "age8",
-                                                   "age9", "age10"), 
-                     labels=c("Less than 5", "5 to 9",
-                              "10 to 14", "15 to 19", "20 to 24", "25 to 34","35 to 44", 
+                                                   "age5", "age6", "age7", "age8"), 
+                     labels=c("Less than 9", "10 to 17", "18 to 24", "25 to 34","35 to 44", 
                               "45 to 54", "55 to 64", "65 and Over")),
       year="2000",
       geoname=stri_trans_general(geoname, id="Title"))
