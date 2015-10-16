555ff3f9678cfce54014652467bb417f6c068d6d
robkemp <rjameskemp@gmail.com>
2015-10-16 14:09:56 -0600

    fixed missing civilian employed persons in emp_cat

--- R/emp_cat.r
+++ R/emp_cat.r
@@ -13,15 +13,15 @@ emp_cat= function(data, empvar="ESR", groups="basic",factor=TRUE){
   require(car, quietly=TRUE)
   gl=list(
     basic= list(
-      recode="2=10;4:5=10; 3=11; 6=12",
+      recode="1:2=10;4:5=10; 3=11; 6=12",
       labels=c("Employed", "Unemployed", "Not In Labor Force"),
       levels=12),
     military=list(
-      recode="2=10;4:5=11; 3=12; 6=13",
+      recode="1:2=10;4:5=11; 3=12; 6=13",
       labels=c("Employed, Civilian","Employed, Armed Forces", "Unemployed", "Not In Labor Force"),
       levels=13),
     lf= list(
-      recode="2:5=10; 6=11",
+      recode="1:2:5=10; 6=11",
       labels=c("In Labor Force", "Not In Labor Force"),
       levels=11))
   grp=gl[[groups]]
